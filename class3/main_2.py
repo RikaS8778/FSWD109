304882d06cb23c00f9d75ec184e0412f225adaa6
rika.s <rika.s8778@gmail.com>
2025-01-27 20:58:43 -0800

    add function for calclation of salary and bonus

--- class3/main_2.py
+++ class3/main_2.py
@@ -1,14 +1,24 @@
+baseSalary = 50000
+# mMultiplier = 1.4
+# eMultiplier = 1.3
+bonusAmount = baseSalary * 0.2
+
 class Employee:
+    multiplier = 1.0
     def __init__(self, name, age, salary):
         self.name = name
         self.age = age
         self.salary = salary
     
     def __str__(self):
-        return f'{self.name} is {self.age} years old and earns {self.salary}'
+        return f'{self.name} is {self.age} years old and earns {self.calc_salary()}'
+    
+    def calc_salary(self):
+        return int(self.salary * self.multiplier)
     
 class Manager(Employee):
     title = 'Manager'
+    multiplier = 1.4
     def __init__(self, name, age, salary, department):
         super().__init__(name, age, salary)
         self.department = department
@@ -16,32 +26,31 @@ class Manager(Employee):
     def __str__(self):
         return super().__str__() + f' working in {self.department} as a {self.title}'
     
+    
 class Executive(Manager):
     title = 'Executive Manager'
+    multiplier = 1.8
     def __init__(self, name, age, salary, department, bonus):
         super().__init__(name, age, salary, department)
         self.bonus = bonus
         
     def __str__(self):
-        return super().__str__() + f' with a bonus of {self.bonus}'
+        return super().__str__() + f' with a bonus of {self.calculate_bonus()}'
     
-    
-baseSalary = 50000
-mMultiplier = 1.4
-eMultiplier = 1.8
-bonusAmount = baseSalary * 0.2
+    def calculate_bonus(self):
+        return int(self.bonus)
     
 e1 = Employee('Amy', 29, baseSalary)
 print(e1)
-e2 = Manager('Joel', 35, int(round(baseSalary*mMultiplier, 0)), 'Sales')
+e2 = Manager('Joel', 35, baseSalary, 'Sales')
 print(e2)
-e3 = Executive('Caitlin', 42, int(round(baseSalary*eMultiplier, 0)), 'Marketing', int(bonusAmount))
+e3 = Executive('Caitlin', 42, baseSalary, 'Marketing', bonusAmount*1.1)
 print(e3)
 
 employees = {
     'Kathy': Employee('Kathy', 26, baseSalary),
-    'Dale': Manager('Dale', 39, baseSalary * mMultiplier, 'Deveopment'),
-    'Micheal': Executive('Micheal', 37, baseSalary * eMultiplier, 'Account', int(bonusAmount))
+    'Dale': Manager('Dale', 39, baseSalary*1.3, 'Deveopment'),
+    'Micheal': Executive('Micheal', 37, baseSalary*1.1, 'Account', bonusAmount*1.15)
 } 
 for x in employees:
     print(employees[x])
